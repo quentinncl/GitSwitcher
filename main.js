f62f0d38103a290dc6ef6eec9d076b11a6f7ceab
quentinnicolle <qnicolle@me.com>
2018-09-30 15:38:36 +0200

    :heavy_plus_sign: Add electron-connect

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import url from 'url'
 import path from 'path'
 import { app, BrowserWindow, Menu } from 'electron'
 
-import { mainMenuTemplate } from './menu'
+// import { mainMenuTemplate } from './menu'
 
 let mainWindow
 
@@ -24,3 +24,74 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu)
 
 })
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { label: 'Add User' },
+            { label: 'Switch User' },
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'close' }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click() { require('electron').shell.openExternal('https://electronjs.org') }
+            }
+        ]
+    }
+]
+
+if (process.platform === 'darwin') {
+    mainMenuTemplate.unshift({
+        label: app.getName(),
+        submenu: [
+            { role: "about" },
+            { type: "separator" },
+            { role: "services", submenu: [] },
+            { type: "separator" },
+            { role: "hide" },
+            { role: "hideothers" },
+            { role: "unhide" },
+            { type: "separator" },
+            { role: "quit" }
+        ]
+    });
+}
